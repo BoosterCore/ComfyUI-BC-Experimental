73c9f1228a3db8d40f9ebbe3286740515cc81ffe
BoosterCore <503072435@qq.com>
2024-07-02 17:21:26 +0800

    Add LoraWithTriggerWord Node
    Tiny rewrite of Extraltodeus' project

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 from .BCE_nodes import ClipTextEncodeBC
 from .BCE_nodes import SaveAnyText
 from .BCE_nodes import SimpleText
+from .BCE_nodes import LoraWithTriggerWord
 
 NODE_CLASS_MAPPINGS = {
     "ClipTextEncodeBC": ClipTextEncodeBC,
     "SaveAnyText": SaveAnyText,
     "SimpleText": SimpleText,
+    "LoraWithTriggerWord": LoraWithTriggerWord,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ClipTextEncodeBC": "🚀 CLIP Text Encode (BCE)",
     "SaveAnyText": "🚀 Save Any Text (BCE)",
     "SimpleText": "🚀 Simple Text (BCE)",
+    "LoraWithTriggerWord": "🚀 Lora With Trigger Word (BCE)",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
